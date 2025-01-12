28d7a8466b794419eeedd873474244058949577d
Eraraya Morenzo Muten <morenzomuten@gmail.com>
2025-01-13 02:37:06 +0700

    Full code convert, not aligned with paper math

--- ESP32S3/main/rsnn.c
+++ ESP32S3/main/rsnn.c
@@ -9,13 +9,12 @@ static const char *TAG = "DEPLOY-UCRSNN";
 // Function implementations
 void rsnn_reset()
 {
-    for (uint16_t i = 0; i < RECURRENT_NEURONS_NUM; ++i)
+    for(uint16_t i = 0; i < RECURRENT_NEURONS_NUM; ++i)
     {
         layer0_v[i] = 0;
         layer0_z[i] = 0;
     }
-    layer0_z_num = 0;
-    for (uint16_t i = 0; i < OUTPUT_NEURONS_NUM; ++i)
+    for(uint16_t i = 0; i < OUTPUT_NEURONS_NUM; ++i)
     {
         layer1_v[i] = 0;
     }
@@ -25,16 +24,19 @@ void rsnn_update(uint16_t *input_z, int32_t *output_potentials, uint16_t input_s
 {
     // Recurrent layer
     apply_leakage(layer0_v, RECURRENT_NEURONS_NUM, ALPHA_DECAY);
-    apply_spikes(layer0_v, layer0_z, NULL, RECURRENT_NEURONS_NUM, RECURRENT_NEURONS_NUM, layer0_z_num, SCALE_FACTOR);
-    apply_spikes(layer0_v, input_z, NULL, RECURRENT_NEURONS_NUM, input_size, input_z_num, SCALE_FACTOR);
-    generate_spikes(layer0_v, layer0_z, RECURRENT_NEURONS_NUM, FIRING_THRESHOLD, &layer0_z_num);
+    // ESP_LOGI(TAG, "    leaks: %ld", layer0_v[0]); //int32_t
+    apply_spikes(layer0_v, layer0_z, layer0_rw, RECURRENT_NEURONS_NUM, RECURRENT_NEURONS_NUM, SCALE_FACTOR);
+    // ESP_LOGI(TAG, "    recur: %ld", layer0_v[0]); //int32_t
+    apply_spikes(layer0_v, input_z, layer0_w, RECURRENT_NEURONS_NUM, input_size, SCALE_FACTOR);
+    // ESP_LOGI(TAG, "    input: %ld", layer0_v[0]); //int32_t
+    generate_spikes(layer0_v, layer0_z, RECURRENT_NEURONS_NUM, FIRING_THRESHOLD);
 
     // Output layer
     apply_leakage(layer1_v, OUTPUT_NEURONS_NUM, KAPPA_DECAY);
-    apply_spikes(layer1_v, layer0_z, NULL, OUTPUT_NEURONS_NUM, RECURRENT_NEURONS_NUM, layer0_z_num, OUTPUT_SCALE_FACTOR);
+    apply_spikes(layer1_v, layer0_z, layer1_w, OUTPUT_NEURONS_NUM, RECURRENT_NEURONS_NUM, OUTPUT_SCALE_FACTOR);
 
     // Save output potentials
-    for (uint16_t i = 0; i < output_size; ++i)
+    for(uint16_t i = 0; i < output_size; ++i)
     {
         output_potentials[i] = layer1_v[i];
     }
@@ -42,37 +44,47 @@ void rsnn_update(uint16_t *input_z, int32_t *output_potentials, uint16_t input_s
 
 void apply_leakage(int32_t* potentials, uint16_t n, uint32_t leakage)
 {
-    int32_t* p = potentials;
-    for(uint16_t i = 0; i < n; ++i)
+    int32_t* p = potentials; //membrane potential of the first neuron in a layer
+    for(uint16_t i = 0; i < n; ++i) //for each neuron in a layer
     {
         int32_t newp = *p*leakage;
 		*(p++) = newp>>15;
     }
 }
 
-void apply_spikes(int32_t *potentials, uint16_t *spikes, int8_t *weights, uint16_t n, uint16_t m, uint16_t spikes_num, uint8_t w_scale)
+void apply_spikes(int32_t* potentials, uint16_t* spikes, int8_t* weights, uint16_t n, uint16_t m, uint8_t w_scale)
 {
-    for (uint16_t j = 0; j < spikes_num; ++j)
-    {
-        for (uint16_t i = 0; i < n; ++i)
-        {
-            potentials[i] += (1 << w_scale); // Simplified weight application
-        }
-    }
+    uint16_t* s = spikes; //spike value of the first source neuron
+	for(uint16_t j = 0; j < m; ++j) //for each source neuron
+	{		
+		int32_t* p = potentials; //membrane potential of the first destination neuron
+		int8_t* w = weights+j*n; //pick weights row for the source neuron
+
+		for(uint16_t i = 0; i < n; ++i) //for each destination neuron
+		{
+            if(*s) //if the source neuron spiked
+            {
+                *(p++) += *(w++)<<w_scale; //apply weights to all the neurons corresponding to the spike
+            }
+		}
+        ++s; //increment index of source neuron
+	}
 }
 
-void generate_spikes(int32_t* potentials, uint16_t* spikes, uint16_t n, int32_t threshold, uint16_t* spikes_num_p)
+void generate_spikes(int32_t* potentials, uint16_t* spikes, uint16_t n, int32_t threshold)
 {
-    int32_t* p = potentials; //membrane potential of the first neuron
-	uint16_t* s = spikes; //spike value of the first neuron
-    *spikes_num_p = 0;
-    for(uint16_t i = 0; i < n; ++i) // for each neuron
+    int32_t* p = potentials; //membrane potential of the first neuron in a layer
+	uint16_t* s = spikes; //spike value of the first neuron in a layer
+    for(uint16_t i = 0; i < n; ++i) // for each neuron in a layer
     {
         if(*p >= threshold)
         {
-            *(s++) = i; //save address
+            *(s++) = 1; //assign spike value for the neuron
             *p -= threshold; //reset membrane potential by substracting threshold
-            (*spikes_num_p)++;
+        }
+        else
+        {
+            *(s++) = 0; //assign spike value for the neuron
         }
         ++p; //increment index of neuron
     }
@@ -84,24 +96,23 @@ void app_main(void)
     ESP_LOGI(TAG, "Initializing RSNN...");
     rsnn_reset();
 
-    uint16_t input_z[1];
-    int32_t output_potentials[1];
+    uint16_t input_z[T][INPUT_NEURONS_NUM] = {{0},{1},{1},{1},{1},{0},{0},{0},{0},{0}};
+    int32_t output_v[T][OUTPUT_NEURONS_NUM] = {{0}};
 
-    while (1)
+    for(uint16_t i = 0; i < T; ++i) // for each timestep
     {
+        ESP_LOGI(TAG, "       t: %d", i); //int32_t
         // Generate random input (0 or 1)
-        input_z[0] = rand() % 2;
+        // input_z[0] = rand() % 2;
+        ESP_LOGI(TAG, " input_z: %d", input_z[i][0]); //uint16_t
         
         // Update RSNN
-        rsnn_update(input_z, output_potentials, 1, 1, input_z[0] ? 1 : 0);
+        rsnn_update(input_z[i], output_v[i], 1, 1, input_z[i][0] ? 1 : 0);
 
         // Print output
-        ESP_LOGI(TAG, "input_z[0]: %d", input_z[0]); //uint16_t
-        ESP_LOGI(TAG, "layer0_v[0]: %ld", layer0_v[0]); //int32_t
-        ESP_LOGI(TAG, "layer0_z[0]: %d", layer0_z[0]); //uint16_t
-        ESP_LOGI(TAG, "layer1_v[0]: %ld", layer1_v[0]); //int32_t
-        ESP_LOGI(TAG, "layer0_z_num: %d", layer0_z_num); //uint16_t
-        ESP_LOGI(TAG, "output_potentials[0]: %ld", output_potentials[0]); //int32_t
+        ESP_LOGI(TAG, "layer0_v: %ld", layer0_v[0]); //int32_t
+        ESP_LOGI(TAG, "layer0_z: %d", layer0_z[0]); //uint16_t
+        ESP_LOGI(TAG, "layer1_v: %ld", layer1_v[0]); //int32_t
         ESP_LOGI(TAG, "----------------------");
 
         // Delay for 1 second
--- ESP32S3/main/rsnn.h
+++ ESP32S3/main/rsnn.h
@@ -5,8 +5,8 @@
 
 // RSNN CONFIG
 
-#define SCALE_FACTOR 1
-#define OUTPUT_SCALE_FACTOR 1
+#define SCALE_FACTOR 0
+#define OUTPUT_SCALE_FACTOR 0
 #define FIRING_THRESHOLD 6  // Spike threshold
 #define ALPHA_DECAY 32767      // Example decay factor for recurrent neurons
 #define KAPPA_DECAY 32767      // Example decay factor for output neurons
@@ -19,21 +19,22 @@
 #define WREC_BUFF_SIZE 1 // RECURRENT_NEURONS_NUM * RECURRENT_NEURONS_NUM
 #define WOUT_BUFF_SIZE 1 // OUTPUT_NEURONS_NUM * RECURRENT_NEURONS_NUM
 
+# define T 10 // total timesteps
+
 /*****/
 // VARIABLES AND BUFFERS
 
 // Weights matrices, encoded row-first (NxM).
 // Each row encodes the interconnections from one input spike.
 // Each column encodes the interconnections to one neuron.
-int8_t layer0_w[WIN_BUFF_SIZE];
-int8_t layer0_rw[WREC_BUFF_SIZE];
-int8_t layer1_w[WOUT_BUFF_SIZE];
+int8_t layer0_w[WIN_BUFF_SIZE] = {3};
+int8_t layer0_rw[WREC_BUFF_SIZE] = {2};
+int8_t layer1_w[WOUT_BUFF_SIZE] = {4};
 
 // Neuron state variables
 int32_t layer0_v[RECURRENT_NEURONS_NUM]; // membrane potentials of recurrent layer
 uint16_t layer0_z[RECURRENT_NEURONS_NUM]; // spiking outputs of recurrent layer
 int32_t layer1_v[OUTPUT_NEURONS_NUM]; // membrane potentials of output layer
-static uint16_t layer0_z_num;
 
 /*****/
 // FUNCTIONS
@@ -45,8 +46,8 @@ void rsnn_update(uint16_t *input_z, int32_t *output_potentials, uint16_t input_s
 // Apply leakage to all the potentials in the input vector.
 void apply_leakage(int32_t *potentials, uint16_t n, uint32_t leakage);
 // Apply spikes proportional to the weights.
-void apply_spikes(int32_t *potentials, uint16_t *spikes, int8_t *weights, uint16_t n, uint16_t m, uint16_t spikes_num, uint8_t w_scale);
+void apply_spikes(int32_t *potentials, uint16_t *spikes, int8_t *weights, uint16_t n, uint16_t m, uint8_t w_scale);
 // Generate spikes given a firing threshold and reset potentials.
-void generate_spikes(int32_t *potentials, uint16_t *spikes, uint16_t n, int32_t threshold, uint16_t *spikes_num_p);
+void generate_spikes(int32_t *potentials, uint16_t *spikes, uint16_t n, int32_t threshold);
 
 #endif // RSNN_H
